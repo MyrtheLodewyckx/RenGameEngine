2677c6de4c0e4ace5d690978a5d7883802126aa2
MyrtheLodewyckx <myrthe.lodewyckx@hotmail.be>
2025-10-03 13:22:48 +0200

    Fix renderer
    Removed Imgui
    Let SDL choose the best accelerated backend
    Enable alpha blending once up front
    General cleanup

--- Rengen/Renderer.cpp
+++ Rengen/Renderer.cpp
@@ -1,107 +1,110 @@
 #include <stdexcept>
+#include <cstring>
 #include "Renderer.h"
 #include "SceneManager.h"
 #include "Texture2D.h"
-#include "backends/imgui_impl_opengl2.h"
-#include "backends/imgui_impl_sdl.h"
+
 
 int GetOpenGLDriverIndex()
 {
-	auto openglIndex = -1;
-	const auto driverCount = SDL_GetNumRenderDrivers();
-	for (auto i = 0; i < driverCount; i++)
-	{
-		SDL_RendererInfo info;
-		if (!SDL_GetRenderDriverInfo(i, &info))
-			if (!strcmp(info.name, "opengl"))
-				openglIndex = i;
-	}
-	return openglIndex;
+    int openglIndex = -1;
+    const int driverCount = SDL_GetNumRenderDrivers();
+    for (int i = 0; i < driverCount; ++i)
+    {
+        SDL_RendererInfo info{};
+        if (SDL_GetRenderDriverInfo(i, &info) == 0)
+        {
+            if (std::strcmp(info.name, "opengl") == 0)
+            {
+                openglIndex = i;
+                break; // stop at first match
+            }
+        }
+    }
+    return openglIndex;
 }
 
 SDL_Window* dae::Renderer::get_window()
 {
-	return m_window;
+    return m_window;
 }
 
 void dae::Renderer::Init(SDL_Window* window)
 {
-	m_window = window;
-	m_renderer = SDL_CreateRenderer(window, GetOpenGLDriverIndex(), SDL_RENDERER_ACCELERATED);
-	if (m_renderer == nullptr) 
-	{
-		throw std::runtime_error(std::string("SDL_CreateRenderer Error: ") + SDL_GetError());
-	}
+    m_window = window;
 
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui_ImplSDL2_InitForOpenGL(window, SDL_GL_GetCurrentContext());
-	ImGui_ImplOpenGL2_Init();
+    const Uint32 flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    m_renderer = SDL_CreateRenderer(window, -1, flags);
+
+    if (!m_renderer)
+        throw std::runtime_error(std::string("SDL_CreateRenderer Error: ") + SDL_GetError());
+
+    SDL_SetRenderDrawBlendMode(m_renderer, SDL_BLENDMODE_BLEND);
 }
 
 void dae::Renderer::Render() const
 {
-	const auto& color = GetBackgroundColor();
-	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
-	SDL_RenderClear(m_renderer);
+    const auto& color = GetBackgroundColor();
+    SDL_SetRenderDrawColor(m_renderer,
+        static_cast<Uint8>(color.r),
+        static_cast<Uint8>(color.g),
+        static_cast<Uint8>(color.b),
+        static_cast<Uint8>(color.a));
+    SDL_RenderClear(m_renderer);
 
-	SceneManager::GetInstance().Render();
-	SceneManager::GetInstance().PostRender();
-	
-	SDL_RenderPresent(m_renderer);
+    SceneManager::GetInstance().Render();
+    SceneManager::GetInstance().PostRender();
+
+    SDL_RenderPresent(m_renderer);
 }
 
 void dae::Renderer::Destroy()
 {
-	ImGui_ImplOpenGL2_Shutdown();
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
-	if (m_renderer != nullptr)
-	{
-		SDL_DestroyRenderer(m_renderer);
-		m_renderer = nullptr;
-	}
+    if (m_renderer)
+    {
+        SDL_DestroyRenderer(m_renderer);
+        m_renderer = nullptr;
+    }
 }
 
-void dae::Renderer::RenderTexture(const Texture2D& texture, const float x, const float y) const
+void dae::Renderer::RenderTexture(const Texture2D& texture, float x, float y) const
 {
-	SDL_Rect dst{};
-	dst.x = static_cast<int>(x);
-	dst.y = static_cast<int>(y);
-	SDL_QueryTexture(texture.GetSDLTexture(), nullptr, nullptr, &dst.w, &dst.h);
-	SDL_RenderCopy(GetSDLRenderer(), texture.GetSDLTexture(), nullptr, &dst);
+    SDL_Rect dst{};
+    dst.x = static_cast<int>(x);
+    dst.y = static_cast<int>(y);
+
+    SDL_QueryTexture(texture.GetSDLTexture(), nullptr, nullptr, &dst.w, &dst.h);
+
+    SDL_RenderCopy(GetSDLRenderer(), texture.GetSDLTexture(), nullptr, &dst);
 }
 
-
-void dae::Renderer::RenderTexture(const Texture2D& texture, float x, float y, float width, float height,
-	SDL_RendererFlip flip) const
+void dae::Renderer::RenderTexture(const Texture2D& texture,
+    float x, float y, float width, float height,
+    SDL_RendererFlip flip) const
 {
-	SDL_Rect dst{};
-	dst.x = static_cast<int>(x);
-	dst.y = static_cast<int>(y);
-	dst.w = static_cast<int>(width);
-	dst.h = static_cast<int>(height);
-	SDL_RenderCopyEx(GetSDLRenderer(), texture.GetSDLTexture(), nullptr, &dst, 0, 0, flip);
+    SDL_Rect dst{
+        static_cast<int>(x), static_cast<int>(y),
+        static_cast<int>(width), static_cast<int>(height)
+    };
+    SDL_RenderCopyEx(GetSDLRenderer(), texture.GetSDLTexture(), nullptr, &dst, 0.0, nullptr, flip);
 }
 
-void dae::Renderer::RenderTexture(const Texture2D& texture, const float x, const float y, const float width,
-	const float height, const float sourceX, const float sourceY, const float sourceWidth, const float sourceHeight,
-	SDL_RendererFlip flip) const
+void dae::Renderer::RenderTexture(const Texture2D& texture,
+    float x, float y, float width, float height,
+    float sourceX, float sourceY, float sourceWidth, float sourceHeight,
+    SDL_RendererFlip flip) const
 {
-	SDL_Rect dst{};
-	dst.x = static_cast<int>(x);
-	dst.y = static_cast<int>(y);
-	dst.w = static_cast<int>(width);
-	dst.h = static_cast<int>(height);
+    SDL_Rect dst{
+        static_cast<int>(x), static_cast<int>(y),
+        static_cast<int>(width), static_cast<int>(height)
+    };
 
-	SDL_Rect srt{};
-	srt.x = static_cast<int>(sourceX);
-	srt.y = static_cast<int>(sourceY);
-	srt.w = static_cast<int>(sourceWidth);
-	srt.h = static_cast<int>(sourceHeight);
+    SDL_Rect src{
+        static_cast<int>(sourceX), static_cast<int>(sourceY),
+        static_cast<int>(sourceWidth), static_cast<int>(sourceHeight)
+    };
 
-	//SDL_RenderCopy(GetSDLRenderer(), texture.GetSDLTexture(), &srt, &dst);
-	SDL_RenderCopyEx(GetSDLRenderer(), texture.GetSDLTexture(), &srt, &dst, 0, 0, flip);
+    SDL_RenderCopyEx(GetSDLRenderer(), texture.GetSDLTexture(), &src, &dst, 0.0, nullptr, flip);
 }
 
 inline SDL_Renderer* dae::Renderer::GetSDLRenderer() const { return m_renderer; }
